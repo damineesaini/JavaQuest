0a111da94d415d7c60dc3ca2664f4c53dc1a42c7
damineesaini <damineesaini11@gmail.com>
2021-07-20 16:19:33 +0530

    updated sortArray

--- src/com/company/SortInLt.java
+++ src/com/company/SortInLt.java
@@ -1,22 +1,39 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class SortInLt {
     public static void main(String[] args){
-        int[] arr = {2,3,1,-9,-3,-4,0,90,-4,6,-5};
 
+        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(2,3,1,-9,-3,-4,0,90,-4,6,-5));
         int temp;
+//        if (arrayList.contains(0))
+//        {
+//            indexOfZero = arrayList.indexOf(0);
+//            for (Integer n:arrayList) {
+//                if (n<0){
+//                    ntvCount++;
+//                }
+//            }
+//            temp = arrayList.get(ntvCount);
 
-        for(int i=0;i< arr.length-1;i++){
-            if (arr[i]>arr[i+1]){
-                temp = arr[i];
-                arr[i]=arr[i+1];
-                arr[i+1]=temp;
-                i=-1;
+  int j=0;
+        for(int i=0; i< arrayList.size();i++) {
+            if (arrayList.get(i)<0){
+                if(j!=i) {
+                    temp = arrayList.get(i);
+                    arrayList.set(i, arrayList.get(j));
+                    arrayList.set(j, temp);
+                }
+                j++;
+
             }
+
         }
 
-        System.out.println("\n Final output: ");
-        for (int a:arr) {
+        for (int a:arrayList
+             ) {
             System.out.print(a+" ");
         }
     }
